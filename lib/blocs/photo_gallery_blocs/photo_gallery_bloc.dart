771ea10940607717f8f7ae8c6d4f1312f70dd865
Alperen Soysal <107396431+alperensoysall@users.noreply.github.com>
2023-08-20 23:55:41 +0300

    photo gallery bloc added and connected to page

---
+++ lib/blocs/photo_gallery_blocs/photo_gallery_bloc.dart
@@ -0,0 +1,58 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:photo_gallery_app/models/api_providers/photo_gallery_api_provider.dart';
+
+import '../../models/data_models/gallery_image_model.dart';
+
+abstract class AbstractPhotoGalleryState {
+  const AbstractPhotoGalleryState();
+}
+
+class PhotoGalleryLoadingState implements AbstractPhotoGalleryState {
+  const PhotoGalleryLoadingState();
+}
+
+class PhotoGalleryLoadedState implements AbstractPhotoGalleryState {
+  final List<GalleryImageModel> galleryImages;
+
+  PhotoGalleryLoadedState({
+    required this.galleryImages,
+  });
+}
+
+class PhotoGalleryErrorState implements AbstractPhotoGalleryState {
+  final String error;
+  PhotoGalleryErrorState(this.error);
+}
+
+abstract class AbstractPhotoGalleryEvent {}
+
+class FetchPhotoGalleryEvent implements AbstractPhotoGalleryEvent {
+  FetchPhotoGalleryEvent();
+}
+
+class PhotoGalleryBloc extends Bloc<AbstractPhotoGalleryEvent, AbstractPhotoGalleryState> {
+  PhotoGalleryBloc() : super(const PhotoGalleryLoadingState()) {
+    on<FetchPhotoGalleryEvent>(_onFetch);
+  }
+
+  Future<void> _onFetch(FetchPhotoGalleryEvent event, Emitter<AbstractPhotoGalleryState> emitter) async {
+    try {
+      emitter(
+        const PhotoGalleryLoadingState(),
+      );
+      final List<Map<String, dynamic>>? postsJson = await PhotoGalleryApiProvider.fetchPhotoGallery();
+      if (postsJson == null) {
+        emitter(PhotoGalleryErrorState("Some error from api"));
+      } else {
+        List<GalleryImageModel> galleryImages = postsJson.map((postAsJson) => GalleryImageModel.fromJSON(postAsJson)).toList();
+        emitter(
+          PhotoGalleryLoadedState(
+            galleryImages: galleryImages,
+          ),
+        );
+      }
+    } catch (e) {
+      emitter(PhotoGalleryErrorState(e.toString()));
+    }
+  }
+}
--- lib/view/pages/home/home.dart
+++ lib/view/pages/home/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:photo_gallery_app/view/pages/home/home_components.dart';
 import 'package:photo_gallery_app/view/pages/home_page/home_page.dart';
+
+import '../../../blocs/photo_gallery_blocs/photo_gallery_bloc.dart';
 
 class HomeTabs extends StatelessWidget {
   const HomeTabs({Key? key}) : super(key: key);
@@ -16,7 +19,10 @@ class HomeTabs extends StatelessWidget {
           CBottomNavigationBarItem(icon: Icons.photo_outlined),
         ],
       ),
-      body: PhotoGalleryHomePage(),
+      body: BlocProvider(
+        create: (BuildContext context) => PhotoGalleryBloc()..add(FetchPhotoGalleryEvent()),
+        child: PhotoGalleryHomePage(),
+      ),
     );
   }
 }
