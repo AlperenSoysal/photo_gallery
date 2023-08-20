8c14f52a0f2498e04ef42417668b0237daedce53
Alperen Soysal <107396431+alperensoysall@users.noreply.github.com>
2023-08-20 22:57:43 +0300

    additional appcolors added

--- lib/view/theme/app_colors.dart
+++ lib/view/theme/app_colors.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   //this is primary app color
   static const Color mainBlue = Colors.blueAccent;
+  static const Color black = Colors.black87;
+  static const Color white = Colors.white54;
 }
