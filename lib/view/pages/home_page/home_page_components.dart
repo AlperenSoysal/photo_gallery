96ad18f1dd8f8243af58871e12f891dcd9daf0b7
Alperen Soysal <107396431+alperensoysall@users.noreply.github.com>
2023-08-21 20:59:28 +0300

    cached_network_image used instead of image.network

--- lib/view/pages/home_page/home_page_components.dart
+++ lib/view/pages/home_page/home_page_components.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:photo_gallery_app/view/main_components.dart';
@@ -29,44 +30,58 @@ class _GalleryImageComponentState extends State<GalleryImageComponent> {
               showId = !showId;
             });
           },
-          child: FutureBuilder<void>(
-            future: precacheImage(NetworkImage(widget.image.url), context),
-            builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                final double randomHeight = Random().nextInt(125) + 75;
-                return ShimmerContainer(width: (MediaQuery.of(context).size.width - 8) / 3, height: randomHeight);
-              } else if (snapshot.hasError) {
-                return ErrorOccurredWidget(
-                  reload: () => context.read<PhotoGalleryBloc>().add(FetchPhotoGalleryEvent()),
-                );
-              } else {
-                return Image.network(widget.image.url);
-              }
+          child: CachedNetworkImage(
+            imageUrl: widget.image.url,
+            errorWidget: (context, _, error) {
+              return ErrorOccurredWidget(
+                reload: () => context.read<PhotoGalleryBloc>().add(FetchPhotoGalleryEvent()),
+              );
+            },
+            placeholder: (context, url) {
+              return ShimmerContainer(width: (MediaQuery.of(context).size.width - 8) / 3, height: 125);
             },
           ),
         ),
-        if (showId)
-          Positioned.fill(
-            left: CSizes.doublePadding,
-            bottom: CSizes.doublePadding,
-            child: Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                decoration: const BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: AppColors.white,
-                      spreadRadius: 5,
-                      blurRadius: 5,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Text(widget.image.id),
-              ),
+        Positioned.fill(
+          left: CSizes.doublePadding,
+          bottom: CSizes.doublePadding,
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: ImageIdComponent(
+              id: widget.image.id,
+              showId: showId,
             ),
           ),
+        ),
       ],
     );
+  }
+}
+
+class ImageIdComponent extends StatelessWidget {
+  final String id;
+  final bool showId;
+
+  const ImageIdComponent({super.key, required this.id, required this.showId});
+
+  @override
+  Widget build(BuildContext context) {
+    if (showId) {
+      return Container(
+        decoration: const BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.white,
+              spreadRadius: 5,
+              blurRadius: 5,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Text(id),
+      );
+    } else {
+      return const SizedBox();
+    }
   }
 }
